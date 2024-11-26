e80ac5d0a82baabe75451b61fbc7b59aeed78f24
nomi3 <thisisnomi3@gmail.com>
2024-11-26 22:36:53 +0900

    Add route

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,20 @@
 import {
   Form,
   Links,
+  Link,
   Meta,
   Scripts,
   ScrollRestoration,
+  Outlet,
 } from "@remix-run/react";
+
+import type { LinksFunction } from "@remix-run/node";
+
+import appStylesHref from "./app.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
 
 export default function App() {
   return (
@@ -36,15 +46,17 @@ export default function App() {
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
             </ul>
           </nav>
         </div>
-
+        <div id="detail">
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
