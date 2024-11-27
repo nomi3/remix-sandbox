f306ee00e3bd29227889f9214eb37fa50f47da3c
nomi3 <thisisnomi3@gmail.com>
2024-11-27 21:26:16 +0900

    Add contacts

--- app/root.tsx
+++ app/root.tsx
@@ -6,17 +6,29 @@ import {
   Scripts,
   ScrollRestoration,
   Outlet,
+  useLoaderData,
 } from "@remix-run/react";
 
 import type { LinksFunction } from "@remix-run/node";
 
 import appStylesHref from "./app.css?url";
+import { getContacts } from "./data";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
 
+export const loader = async () => {
+  const contacts = await getContacts();
+  return new Response(JSON.stringify({ contacts }), {
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  });
+};
+
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -44,14 +56,30 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <Link to={`/contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`/contacts/2`}>Your Friend</Link>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
         <div id="detail">
--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,17 +1,27 @@
-import { Form } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import type { FunctionComponent } from "react";
+import invariant from "tiny-invariant";
 
 import type { ContactRecord } from "../data";
+import { getContact } from "../data";
+
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId");
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return new Response(JSON.stringify({ contact }), {
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  });
+};
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placecats.com/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact } = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
